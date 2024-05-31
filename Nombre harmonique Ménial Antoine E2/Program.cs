88a4c959bf15750107f269bc6c34391dce118087
Antoine <antoinemnl805@gmail.com>
2024-05-31 12:16:14 +0200

    Correction apporté au sein du code

--- Nombre harmonique Ménial Antoine E2/Program.cs
+++ Nombre harmonique Ménial Antoine E2/Program.cs
@@ -1,30 +1,50 @@
 ﻿
-//sous programme demande saisi entier a 
-int Saisi()
+////sous programme demande saisi entier a 
+//int Saisi()
+//{
+//    int a;
+//    Console.WriteLine("Entrez un entier a strictement positif");
+//    a = Convert.ToInt32(Console.ReadLine());
+//    return a;
+//}
+
+int[] CreeTab()
 {
     int a;
-    Console.WriteLine("Entrez un entier a strictement positif");
+    Console.WriteLine("Entrez un entier a strictement positif : ");
     a = Convert.ToInt32(Console.ReadLine());
-    return a;
+    int[] tab = new int[a];
+
+    return tab;
+
 }
 
 
-//Création suite harmonique
-int NbHarmonique(int[] tab)
+////Création suite harmonique
+//int NbHarmonique(int[] tab)
+//{
+//    int val = 1;
+//    for (int i = 1; i < tab.Length; i++)
+//    {
+//        val = val + 1 / i;
+//    }
+//    return val;
+//}
+
+
+////Création du tableau
+//int[] CréerTab(int taille)
+//{
+//    return new int[taille];
+//}
+
+void InitTab(int[] tab)
 {
     int val = 1;
-    for (int i = 1; i < tab.Length; i++)
+    for (int i = 0; i < tab.Length; i++)
     {
-        val = val + 1 / i;
+        tab[i] = val + 1 / val;
     }
-    return val;
-}
-
-
-//Création du tableau
-int[] CréerTab(int taille)
-{
-    return new int[taille];
 }
 
 
@@ -38,6 +58,13 @@ void AfficherTab(int[] tab)
         Console.Write(tab[i] + " ");
     }
 }
+
+
+int[] tab = CreeTab();
+InitTab(tab);
+AfficherTab(tab);
+
+
 
 
 
