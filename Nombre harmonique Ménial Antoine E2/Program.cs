8287db8cd6ec205b54b6cf210dd14d61f4fb540c
Antoine <antoinemnl805@gmail.com>
2024-05-31 12:00:10 +0200

    Correction du sous programme NbHarmonique

--- Nombre harmonique Ménial Antoine E2/Program.cs
+++ Nombre harmonique Ménial Antoine E2/Program.cs
@@ -8,31 +8,37 @@ int Saisi()
     return a;
 }
 
+
+//Création suite harmonique
+int NbHarmonique(int[] tab)
+{
+    int val = 1;
+    for (int i = 1; i < tab.Length; i++)
+    {
+        val = val + 1 / i;
+    }
+    return val;
+}
+
+
 //Création du tableau
 int[] CréerTab(int taille)
 {
     return new int[taille];
 }
 
-//Création suite harmonique
-int[] NbHarmonique(int[] tab)
-{
-    int val = 0;
-    for (int i = 1; i < tab.Length; i++)
-    {
-        val = val + 1 / tab[i];
-    }
-    return val;
-}
+
 
 //Création sous-programme pour afficher le tableau
 void AfficherTab(int[] tab)
 {
+    Console.WriteLine("Le tableau résultat contient :");
     for (int i = 0; i < tab.Length; i++)
     {
         Console.Write(tab[i] + " ");
     }
 }
+
 
 
 
