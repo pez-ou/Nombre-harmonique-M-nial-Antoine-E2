82df9daf2c03f4bb8f3ceb518485f90ca37f86b3
Antoine <antoinemnl805@gmail.com>
2024-05-31 11:40:41 +0200

    Sous programme de saisi entier a

--- Nombre harmonique Ménial Antoine E2/Program.cs
+++ Nombre harmonique Ménial Antoine E2/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+//sous programme demande saisi entier a 
+int Saisi()
+{
+    int a;
+    Console.WriteLine("Entrez un entier a strictement positif");
+    a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+
